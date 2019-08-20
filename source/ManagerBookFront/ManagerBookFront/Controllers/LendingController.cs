f576209c65353f96fc2f2c7b8c190dc7ff19acd2
Jéssica Dias <jeh_dias18@hotmail.com>
2019-08-19 21:58:18 -0300

    Add Front

---
+++ source/ManagerBookFront/ManagerBookFront/Controllers/LendingController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ManagerBookFront.Controllers
+{
+    public class LendingController : Controller
+    {
+        // GET: Lending
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
